ad8fe5b93a6fc706dfba05eb47c2c661fc0b62d3
FookerRojer <gulenkov.dmitriy@mail.ru>
2024-10-31 19:21:38 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Heade } from "./components/Heade/Heade";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Heade />
+      </header>
+    </>
+  );
+};
+export default App;
---
+++ src/components/Heade/Heade.jsx
@@ -0,0 +1,10 @@
+import './Heade.css';
+
+export function Heade (){
+	return (
+		<>
+			<div class="heade"></div>
+		</>
+	);
+};
+// style={ "width: 100%; height: 55px;"}
